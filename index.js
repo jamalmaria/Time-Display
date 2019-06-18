bfe57a1814ea504771d1536d024f62406b2e0918
jamalmaria <jamalmaria111@gmail.com>
2019-06-18 16:14:43 +0530

    UI Change

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function showClock(){
     var btnTime = document.getElementById('btn-time');
     var clockDiv = document.getElementById('clock');
     btnTime.style.display = "none";
-    clockDiv.style.paddingTop = "25%";
+    clockDiv.style.paddingTop = "15%";
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     setInterval(function(){
         var date = new Date();
         clockDiv.innerHTML = `<h1 class="time">
-          ${date.getDate()}/ ${date.getMonth()}/ ${date.getFullYear()} <br>
+          ${days[date.getDay()]}<br> ${date.getDate()}/ ${date.getMonth()}/ ${date.getFullYear()} <br>
          ${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}</h1>`;
     },1000);
 
